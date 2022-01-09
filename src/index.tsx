001d9d0957ab04ca77c91bd94179dcc3b10cc57e
SECT <info@sectwebstudio.com>
2022-01-10 03:50:59 +0900

    Add package 'sanitize.css'

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/base/settings.css';
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
 import './index.css';
 import App from './App';
 
