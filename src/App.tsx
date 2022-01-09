449b50ecc908f5e73ffc5abcbb923bd369d2084c
SECT <info@sectwebstudio.com>
2022-01-10 01:26:21 +0900

    Replae components

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import Layout from './components/Layout';
+
+interface MyData {
+  message: string;
+}
+
+interface Users {
+  id: number;
+  name: string;
+  email: string;
+  verified: boolean;
+}
+
+declare global {
+  interface Window {
+    my_data: MyData;
+  }
+}
+
+const App: React.VFC = () => {
+  const [users] = useState<Users[]>([
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@google.com',
+      verified: false,
+    },
+    {
+      id: 2,
+      name: 'Jack',
+      email: 'jack@google.com',
+      verified: true,
+    },
+    {
+      id: 3,
+      name: 'James',
+      email: 'james@google.com',
+      verified: false,
+    },
+  ]);
+
+  return (
+    <Layout>
+      <ul className="user-list">
+        {users?.map((user) => (
+          <li key={user.id} className={user.verified ? 'verified' : ''}>
+            {user.name}
+          </li>
+        ))}
+      </ul>
+      <div className="icons">
+        <svg className="icon">
+          <use xlinkHref="#icon-facebook" />
+        </svg>
+        <svg className="icon">
+          <use xlinkHref="#icon-twitter" />
+        </svg>
+        <svg className="icon">
+          <use xlinkHref="#icon-youtube" />
+        </svg>
+        <svg className="icon">
+          <use xlinkHref="#icon-instagram" />
+        </svg>
+        <svg className="icon">
+          <use xlinkHref="#icon-line" />
+        </svg>
+        <svg className="icon">
+          <use xlinkHref="#icon-arrow-top" />
+        </svg>
+        <svg className="icon">
+          <use xlinkHref="#icon-search" />
+        </svg>
+      </div>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+const Header: React.VFC = () => (
+  <header className="header">
+    <h1 className={styles.heading}>App Component</h1>
+  </header>
+);
+
+export default Header;
