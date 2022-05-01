098067a8c5d81fb274924c82ec6c482a51781374
SECT <info@sectwebstudio.com>
2022-05-02 01:38:02 +0900

    Migrate React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import '@/assets/css/base/settings.css';
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
@@ -7,9 +9,12 @@ import 'sanitize.css/typography.css';
 import '@/index.css';
 import App from './App';
 
-ReactDOM.render(
+const el = document.getElementById('root');
+if (el === null) throw new Error('Root container missing in index.html');
+
+const root = ReactDOM.createRoot(el);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
