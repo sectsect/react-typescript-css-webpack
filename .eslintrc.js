00f73ba9f798f0505f93413982e7dd4b887d071e
SECT <info@sectwebstudio.com>
2022-05-02 01:48:23 +0900

    Fix warning for React version on ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   settings: {
     react: {
-      version: 'latest',
+      version: '"detect',
     },
     jest: { version: 27 },
     // 'import/ignore': ['.css$', 'node_modules/*'],
