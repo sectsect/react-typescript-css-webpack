a7531e05e490761e2cb3a00b54ba3c2302b9a15b
SECT <sect@sect.me>
2022-07-15 03:18:30 +0900

    Code refactor

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,7 +7,11 @@ import { fetchPosts } from '@/utils/requests/post';
 
 const Home: React.FC = () => {
   // const { data, isLoading, isError, error } = useQuery(
-  const { data, isLoading, error } = useQuery('posts', () => fetchPosts(), {
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useQuery('posts', () => fetchPosts(), {
     // cacheTime: 1000, // Defaults to 5 * 60 * 1000 (5 minutes)
     // staleTime: Infinity,
     // refetchOnWindowFocus: false,
@@ -29,8 +33,6 @@ const Home: React.FC = () => {
       </section>
     );
   }
-
-  const posts = data;
 
   return (
     <>
